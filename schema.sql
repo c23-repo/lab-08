85e088ba32b27233c974349de44ea7126f2c9a20
Jorie Fernandez <joriefefernandez@yahoo.com>
2019-05-02 23:00:35 -0700

    Modified server.js

--- schema.sql
+++ schema.sql
@@ -1,23 +1,26 @@
-CREATE TABLE location (
-  latitude DECIMAL NOT NULL,
-  longitude DECIMAL NOT NULL,
-  formatted_query VARCHAR(255) NOT NULL,
-  search_query VARCHAR(255) NOT NULL,
-  id SERIAL PRIMARY KEY
-);
 
-CREATE TABLE weather (
-  summary TEXT NOT NULL,
-  post_date DATE NOT NULL,
-  location_id INTEGER NOT NULL, 
-  id SERIAL PRIMARY KEY
-);
+DROP TABLE IF EXISTS weathers, yelps, movies, trails, locations, events;
 
-CREATE TABLE events (
-  location_id INTEGER NOT NULL,
-  link TEXT NOT NULL,
-  event_name TEXT NOT NULL,
-  event_date DATE NOT NULL,
-  summary TEXT NOT NULL,
-  id SERIAL PRIMARY KEY
+CREATE TABLE IF NOT EXISTS locations ( 
+    id SERIAL PRIMARY KEY, 
+    search_query VARCHAR(255), 
+    formatted_query VARCHAR(255), 
+    latitude NUMERIC(10, 7), 
+    longitude NUMERIC(10, 7)
+  );
+
+CREATE TABLE IF NOT EXISTS weathers ( 
+    id SERIAL PRIMARY KEY, 
+    forecast VARCHAR(255), 
+    time VARCHAR(255),
+    location_id INTEGER NOT NULL REFERENCES locations(id) 
+  );
+
+CREATE TABLE IF NOT EXISTS events (
+  id SERIAL PRIMARY KEY, 
+  link VARCHAR(255), 
+  name VARCHAR(255), 
+  summary VARCHAR(1000),
+  event_date CHAR(15), 
+  location_id INTEGER NOT NULL REFERENCES locations(id) 
 );
